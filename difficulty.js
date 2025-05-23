d322fd6819047ef92c41bd95949c80f6dd116335
Robin <161661738+Robin6807@users.noreply.github.com>
2025-05-23 10:21:45 -0400

    Add files via upload

--- difficulty.js
+++ difficulty.js
@@ -2,11 +2,16 @@ window.addEventListener("load",addListener);
 
 function addListener()
 {
-	document.getElementById("btnmiddleschool").addEventListener("click",gohtml);
-	document.getElementById("btnhighschool").addEventListener("click",gohtml);
+	document.getElementById("btnmiddleschool").addEventListener("click",gohighhtml);
+	document.getElementById("btnhighschool").addEventListener("click",gomiddlehtml);
 }
 
-function gohtml()
+function gohighhtml()
 {
-	window.location.href = "piinput.html";
+	window.location.href = "piinputhard.html";
+}
+
+function gomiddlehtml()
+{
+	window.location.href = "piinputmiddle.html";
 }
