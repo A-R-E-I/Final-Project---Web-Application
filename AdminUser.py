2438ae9cbb58678287e01f31ddd43ed01e2dffdd
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2025-06-10 16:35:51 -0400

    Add files via upload

--- AdminUser.py
+++ AdminUser.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, redirect
+from os import path
 import os.path
 import sys
-from os import path
+
 
 global whichfilename;
 whichfilename = "LoginAdmin.doc";
@@ -15,23 +16,15 @@ def main():
 
 @app.route("/select",methods=["POST"])
 def WhichAccount():
-    global account
+    UserExit = request.form.get("ExitYN")
+    if(UserExit == "entyes"):
+        return render_template("EndPro.html");
+    
     account = request.form.get("WhatAccount")
-    YNexit();
     if(account == "AdminAct"):
         return render_template("ExtractAdmin.html");
     else:
         return render_template("ExtractUser.html");
-
-def YNexit():
-    UserExit = request.form.get("ExitYN")
-    if(UserExit == "entyes"):
-        return render_template("EndPro.html");
-        sys.exit()
-    else:
-        return render_template("AdminUser.html")
-    
-
-    
+     
 if __name__=="__main__":
     app.run();
